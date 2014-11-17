247a0e4d3f2c3654ad69d1ac64c9451c7d4a7777
Kevin Jakob <Kevin.Jakob@Student.uni-augsburg.de>
2014-11-17 00:00:00 +0000

    version 1.0

---
+++ R/cpploss.R
@@ -0,0 +1,4 @@
+cpploss <- function(default_distr_a, link_function_a, S_a, Sigma_a, W_a, PD_a, PL_a, calc_rc_a, loss_thr_a, seed_a, max_entries_a) {
+  .Call('GCPM_cpploss', PACKAGE = 'GCPM', default_distr_a, link_function_a, S_a, Sigma_a, W_a, PD_a, PL_a, calc_rc_a, loss_thr_a, seed_a, max_entries_a)
+}
+
